5764b9553010128ec90c1f34fdc568cd243b1214
NanJiu <nanjiu533@163.com>
2024-10-08 11:06:51 +0800

    update

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_meizu21Pro.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_meizu21Pro-user \
+    omni_meizu21Pro-userdebug \
+    omni_meizu21Pro-eng
